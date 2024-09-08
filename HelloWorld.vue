df021076ab35f8c96270c94a2c9c7e8b8339f3fb
Azmat136 <1360641701@qq.com>
2024-09-08 16:09:47 +0800

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h3>跨域解决方案</h3>
+    <p>{{ title }}</p>
+    <p>{{ wenduan.message }}</p>
+
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+// "https://jsonplaceholder.typicode.com/posts/1"
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      title:"hello",
+      wenduan:[]
+    }
+  },
+  mounted(){
+    axios.get("/api/data").then(res =>{
+      this.title=res.data.title;
+      this.wenduan=res.data;
+      console.log(res.data.text);
+    });
+  },
+}
+</script>
